dd327677c7980b75a7203b8cf197aa146eaedcc8
William Oldham <wegg7250@gmail.com>
2018-11-10 21:43:19 +0000

    More exit codes

--- src/main/java/com/cascadebot/shared/ExitCodes.java
+++ src/main/java/com/cascadebot/shared/ExitCodes.java
@@ -5,7 +5,8 @@ public final class ExitCodes {
     public static int STOP = 20;
     public static int FORCE_STOP = 21; // Not sure if this would be applicable, kill all threads and exit?
     public static int UPDATE = 22;
-    public static int ERROR_STOP_NO_RESTART = 23; // Don't restart on stop (Config errors, multiple login failures etc)
-    public static int ERROR_STOP_RESTART = 24; // Recoverable error (Restart after stop)
+    public static int RESTART = 23;
+    public static int ERROR_STOP_NO_RESTART = 24; // Don't restart on stop (Config errors, multiple login failures etc)
+    public static int ERROR_STOP_RESTART = 25; // Recoverable error (Restart after stop)
 
 }
