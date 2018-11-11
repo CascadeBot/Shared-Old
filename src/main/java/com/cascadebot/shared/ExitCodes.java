03c259fe88aef74674fd6ea90c6e0967efe3760c
William Oldham <wegg7250@gmail.com>
2018-11-11 10:36:23 +0000

    Remove force stop

--- src/main/java/com/cascadebot/shared/ExitCodes.java
+++ src/main/java/com/cascadebot/shared/ExitCodes.java
@@ -3,10 +3,9 @@ package com.cascadebot.shared;
 public final class ExitCodes {
 
     public static int STOP = 20;
-    public static int FORCE_STOP = 21; // Not sure if this would be applicable, kill all threads and exit?
-    public static int UPDATE = 22;
-    public static int RESTART = 23;
-    public static int ERROR_STOP_NO_RESTART = 24; // Don't restart on stop (Config errors, multiple login failures etc)
-    public static int ERROR_STOP_RESTART = 25; // Recoverable error (Restart after stop)
+    public static int UPDATE = 21;
+    public static int RESTART = 22;
+    public static int ERROR_STOP_NO_RESTART = 23; // Don't restart on stop (Config errors, multiple login failures etc)
+    public static int ERROR_STOP_RESTART = 24; // Recoverable error (Restart after stop)
 
 }
