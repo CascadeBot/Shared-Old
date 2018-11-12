7fd5591a9b605d1ad422f2bc4adbba44d06670d0
William Oldham <wegg7250@gmail.com>
2018-11-12 17:46:09 +0000

    Constants

--- src/main/java/com/cascadebot/shared/ExitCodes.java
+++ src/main/java/com/cascadebot/shared/ExitCodes.java
@@ -2,10 +2,10 @@ package com.cascadebot.shared;
 
 public final class ExitCodes {
 
-    public static int STOP = 20;
-    public static int UPDATE = 21;
-    public static int RESTART = 22;
-    public static int ERROR_STOP_NO_RESTART = 23; // Don't restart on stop (Config errors, multiple login failures etc)
-    public static int ERROR_STOP_RESTART = 24; // Recoverable error (Restart after stop)
+    public static final int STOP = 20;
+    public static final int UPDATE = 21;
+    public static final int RESTART = 22;
+    public static final int ERROR_STOP_NO_RESTART = 23; // Don't restart on stop (Config errors, multiple login failures etc)
+    public static final int ERROR_STOP_RESTART = 24; // Recoverable error (Restart after stop)
 
 }
