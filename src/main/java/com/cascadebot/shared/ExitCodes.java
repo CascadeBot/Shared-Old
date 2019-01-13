ddb06472aab083b3279fc6b38cc642d93a384bc4
William Oldham <wegg7250@gmail.com>
2019-01-13 10:15:28 +0000

    Add extra exit code

--- src/main/java/com/cascadebot/shared/ExitCodes.java
+++ src/main/java/com/cascadebot/shared/ExitCodes.java
@@ -13,5 +13,6 @@ public final class ExitCodes {
     public static final int RESTART = 22;
     public static final int ERROR_STOP_NO_RESTART = 23; // Don't restart on stop (Config errors, multiple login failures etc)
     public static final int ERROR_STOP_RESTART = 24; // Recoverable error (Restart after stop)
+    public static final int STOPPED_BY_WRAPPER = 25;
 
 }
