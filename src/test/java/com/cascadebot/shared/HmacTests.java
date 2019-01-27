a22f7b27580ba8a3b4b741c82303ce869deab5be
weeryan17 <theweeryan17@gmail.com>
2019-01-27 11:14:30 -0600

    Change up the test a bit

--- src/test/java/com/cascadebot/shared/HmacTests.java
+++ src/test/java/com/cascadebot/shared/HmacTests.java
@@ -14,6 +14,7 @@ class HmacTests {
             Auth auth = new Auth("key1234"); //Not out actual key
             String hmacText = auth.hmacEncrypt("Test");
             assertTrue(auth.verifyEncrypt("Test", hmacText));
+            assertFalse(auth.verifyEncrypt("Test2", hmacText));
         } catch (NoSuchAlgorithmException | InvalidKeyException ignored) {
         }
     }
