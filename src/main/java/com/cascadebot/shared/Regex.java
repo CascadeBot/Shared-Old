dc9a8aaff9ca26087e016805200a9b8cdeda14c6
William Oldham <wegg7250@gmail.com>
2019-02-21 09:35:38 +0000

    Add discord regex

--- src/main/java/com/cascadebot/shared/Regex.java
+++ src/main/java/com/cascadebot/shared/Regex.java
@@ -15,4 +15,11 @@ public final class Regex {
     public static final Pattern POSITIVE_DECIMAL_REGEX = Pattern.compile("[0-9]*([.,])[0-9]+");
     public static final Pattern MULTISPACE_REGEX = Pattern.compile(" {2,}");
 
+    public static final Pattern ID = Pattern.compile("[0-9]{17,}");
+    public static final Pattern USER_MENTION = Pattern.compile("<@!?([0-9]{17,})>");
+    public static final Pattern ROLE_MENTION = Pattern.compile("<@&([0-9]{17,})>");
+    public static final Pattern CHANNEL_MENTION = Pattern.compile("<#([0-9]{17,})>");
+    public static final Pattern EMOTE_MENTION = Pattern.compile("<:(.+):([0-9]{17,})>");
+    public static final Pattern ANIMATED_EMOTE_MENTION = Pattern.compile("<a:(.+):([0-9]{17,})>");
+
 }
